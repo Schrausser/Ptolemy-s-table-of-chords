83fd0231ec35ec7fa348cc4f66979051c129d7bc
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2025-01-05 05:12:25 +0100

    Create chord_geo.pas

---
+++ chord_geo.pas
@@ -0,0 +1,10 @@
+GA:= circle(0,10); // c(FF000000) v(1) 
+GB:= element(GA,2.19348870081); // c(FF000000) v(1) a(t(1) s(3) p(3) b(0) e(6.2832) S(0.2)) 
+GC:= point(10,0); // c(FF000000) v(1) 
+GE:= half_line(0,GB); // c(FF000000) 
+GD:= segment(0,GB); // c(FF000000) v(1) 
+GG:= segment(GB,GC); // c(FF000000) v(1) 
+
+radius(GA)= 10.;  //   ff000000
+angle(0,GC,GB)= 125.677644966;  // x ff000000
+distance(GB,GC)= 17.7945177916;  // x ff000000
